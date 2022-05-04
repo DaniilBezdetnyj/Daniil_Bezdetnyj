a4a77ef0f1d1d7aabb313c1e01845c15852b47fb
Daniil Bezdetnyj <bezdetnyj@dlit.dp.ua>
2022-05-04 14:49:41 +0300

    Add Task_1

---
+++ Hometask_Basic_Knowledge/Extra_Task_2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+
+namespace Hometask_Basic_Knowledge
+{
+    [TestFixture]
+    public class Extra_Task_2
+    {
+        public string IP_address(long k)
+        {
+            string[] str1 = { ((k - (((k - k%256 - (((k- k % 256) / (256)))%256)/(256*256))%256) - (((k- k % 256) / (256))%256) - k%256)/(256*256*256)).ToString(),
+                    (((k - k%256 - (((k- k % 256) / (256)))%256)/(256*256))%256).ToString(),
+                    (((k- k % 256) / (256))%256).ToString(),
+                    (k%256).ToString()};
+
+            return string.Join('.', str1);
+        }
+        [Test]
+        public void Test_IP1()
+        {
+            Assert.AreEqual("128.32.10.1", IP_address(2149583361));
+        }
+
+        [Test]
+        public void Test_IP2()
+        {
+            Assert.AreEqual("0.0.0.32", IP_address(32));
+        }
+        [Test]
+        public void Test_IP3()
+        {
+            Assert.AreEqual("0.0.0.0", IP_address(0));
+        }
+    }
+}
